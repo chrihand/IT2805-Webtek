fc2a25b6ee5f5e872f2a67a1982f1a8c0baf88c4
Christina Hellenes Andresen <chrihand@stud.ntnu.no>
2015-11-01 22:33:45 +0100

    Endra css, products og laget mouseover javascript

--- Glowee/scripts/rollOverMenu.js
+++ Glowee/scripts/rollOverMenu.js
@@ -1,59 +1,23 @@
-var menuItem = 0;
+var menuItem;
+var timer;
+document.onclick = mouseClose;
 
 function mouseOpen(id) {
-	if (menuItem) menuItem.style.visibility = 'hidden';
-
+	console.log(id)
+	if (menuItem) menuItem.style.visibility = "hidden";
 	menuItem = document.getElementById(id);
-	menuItem.style.visibility = 'visible';
+	menuItem.style.visibility = "visible";
 }
 
 function mouseClose() {
-	if (menuItem) menuItem.style.visibility = 'hidden';
+	timer = window.setTimeout(close, 500);
 }
 
-
-
-
-
-/*var timeout	= 500;
-var closetimer	= 0;
-var ddmenuitem	= 0;
-
-// open hidden layer
-function mopen(id)
-{	
-	// cancel close timer
-	mcancelclosetime();
-
-	// close old layer
-	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
-
-	// get new layer and show it
-	ddmenuitem = document.getElementById(id);
-	ddmenuitem.style.visibility = 'visible';
-
-}
-// close showed layer
-function mclose()
-{
-	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
+function close() {
+	if (menuItem) menuItem.style.visibility = "hidden";
 }
 
-// go close timer
-function mclosetime()
-{
-	closetimer = window.setTimeout(mclose, timeout);
+function keepSubmenu() {
+	window.clearTimeout(timer);
 }
 
-// cancel close timer
-function mcancelclosetime()
-{
-	if(closetimer)
-	{
-		window.clearTimeout(closetimer);
-		closetimer = null;
-	}
-}
-
-// close layer when click-out
-document.onclick = mclose; */
