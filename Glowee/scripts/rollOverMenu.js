593dd031dcdc3a831f4526f4dca94ba141aed8e3
Christina Hellenes Andresen <chrihand@stud.ntnu.no>
2015-11-01 14:32:27 +0100

    Added scripts and products.html

---
+++ Glowee/scripts/rollOverMenu.js
@@ -0,0 +1,59 @@
+var menuItem = 0;
+
+function mouseOpen(id) {
+	if (menuItem) menuItem.style.visibility = 'hidden';
+
+	menuItem = document.getElementById(id);
+	menuItem.style.visibility = 'visible';
+}
+
+function mouseClose() {
+	if (menuItem) menuItem.style.visibility = 'hidden';
+}
+
+
+
+
+
+/*var timeout	= 500;
+var closetimer	= 0;
+var ddmenuitem	= 0;
+
+// open hidden layer
+function mopen(id)
+{	
+	// cancel close timer
+	mcancelclosetime();
+
+	// close old layer
+	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
+
+	// get new layer and show it
+	ddmenuitem = document.getElementById(id);
+	ddmenuitem.style.visibility = 'visible';
+
+}
+// close showed layer
+function mclose()
+{
+	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
+}
+
+// go close timer
+function mclosetime()
+{
+	closetimer = window.setTimeout(mclose, timeout);
+}
+
+// cancel close timer
+function mcancelclosetime()
+{
+	if(closetimer)
+	{
+		window.clearTimeout(closetimer);
+		closetimer = null;
+	}
+}
+
+// close layer when click-out
+document.onclick = mclose; */
