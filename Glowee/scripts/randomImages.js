9723298cc55ab112cf7daf52dd88cb5c962b205f
Ingrid <ingrid.skar@hotmail.com>
2015-11-02 16:16:40 +0100

    La til js

---
+++ Glowee/scripts/randomImages.js
@@ -0,0 +1,13 @@
+var mySlideShow = document.getElementById("myPhoto");
+var imageArray = ["images/GloweeAccessories.png", "images/GloweeSticks.png", "images/GloweeGlasses.png", "images/GloweeHat.png"]
+var imageIndex = 0;
+
+function changeImage() {
+	myPhoto.setAttribute("src", imageArray[ imageIndex]);
+	imageIndex ++;
+	if (imageIndex>=imageArray.length) {
+		imageIndex = 0;
+	}
+}
+
+var intervalHandle = setInterval(changeImage,2000);
