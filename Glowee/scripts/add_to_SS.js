86ef66759a98e4b18029ce72a4f45cbf54bdc570
Henrik <henrikbossart@me.com>
2015-11-04 11:29:24 +0100

    lagt til god morgen

---
+++ Glowee/scripts/add_to_SS.js
@@ -0,0 +1,11 @@
+var amountSource = document.getElementById("amount");
+var buyButton  = document.getElementById("buyButton");
+var price = document.getElementById("price").innerHTML;
+
+var amount = amountSource.value;
+
+buyButton.addEventListener("click", console.log(amount));
+
+function add_to_SS () {
+    
+}
