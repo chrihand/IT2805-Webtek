db040c77fb52e7af99781fd598c69037bba50708
Henrik <henrikbossart@me.com>
2015-11-12 13:11:28 +0100

    la til bilder i slideshow og endret litt på koden; la til eventhandler på window slik at den kjører når siden laster inn

--- Glowee/scripts/randomImages.js
+++ Glowee/scripts/randomImages.js
@@ -1,13 +1,16 @@
-var mySlideShow = document.getElementById("slideShow");
-var imageArray = ["images/GloweeAccessories.png", "images/GloweeSticks.png", "images/GloweeGlasses.png", "images/GloweeHat.png"]
-var imageIndex = 0;
+window.addEventListener("load", function() {
 
-function changeImage() {
-	myPhoto.setAttribute("src", imageArray[ imageIndex]);
-	imageIndex ++;
-	if (imageIndex>=imageArray.length) {
-		imageIndex = 0;
-	}
-}
+    var mySlideShow = document.getElementById("slideShow");
+    var imageArray = ["../images/GloweeSlideshow1.png", "../images/GloweeSlideshow2.png", "../images/GloweeSlideshow1.png", "../images/GloweeSlideshow2.png"]
+    var imageIndex = 0;
 
-var intervalHandle = setInterval(changeImage,2000);
+    function changeImage() {
+        myPhoto.setAttribute("src", imageArray[ imageIndex]);
+        imageIndex ++;
+        if (imageIndex>=imageArray.length) {
+            imageIndex = 0;
+        }
+    }
+
+    var intervalHandle = setInterval(changeImage,2000);
+});
