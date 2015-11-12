540d8262757e85ba52b000331e687a9db8d31f17
Henrik <henrikbossart@me.com>
2015-11-12 13:41:10 +0100

    fikset slideshowet

--- Glowee/scripts/randomImages.js
+++ Glowee/scripts/randomImages.js
@@ -1,11 +1,10 @@
-window.addEventListener("load", function() {
 
     var mySlideShow = document.getElementById("slideShow");
     var imageArray = ["../images/GloweeSlideshow1.png", "../images/GloweeSlideshow2.png", "../images/GloweeSlideshow1.png", "../images/GloweeSlideshow2.png"]
     var imageIndex = 0;
 
     function changeImage() {
-        myPhoto.setAttribute("src", imageArray[ imageIndex]);
+        mySlideShow.setAttribute("src", imageArray[ imageIndex]);
         imageIndex ++;
         if (imageIndex>=imageArray.length) {
             imageIndex = 0;
@@ -13,4 +12,3 @@ window.addEventListener("load", function() {
     }
 
     var intervalHandle = setInterval(changeImage,2000);
-});
