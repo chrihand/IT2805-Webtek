a26d03cc421407569d68d5f7def37fa28da7c95a
Christina Hellenes Andresen <chrihand@stud.ntnu.no>
2015-11-15 22:42:23 +0100

    Fikset en hel del, mest kommentarer

--- Glowee/scripts/contact.js
+++ Glowee/scripts/contact.js
@@ -1,19 +1,28 @@
 window.addEventListener("load",function(){
 	document.getElementById("kontaktSkjema").addEventListener("submit",validerSkjema);
 	
+	/* Function witch validated the three required fields.
+	If everything is valid, a message with "The message is sendt" appears. 
+	*/
 	function validerSkjema(){
+
+		// Validates name
 		var navnet = document.forms["kontaktform"]["name"].value;
 		if (navnet == null || navnet == ""){
 			document.getElementById('navnFelt').style.borderColor = "red";
 			alert("dette feltet må være utfylt");
 			return false;
 		}
+
+		// Validates e-mail
 		var epost = document.forms["kontaktform"]["email"].value;
 		if (epost == null || epost == ""){
 			document.getElementById('epost').style.borderColor = "red";
 			alert("dette feltet må være utfylt");
 			return false;
 		}
+
+		// Validates comment field
 		var kfelt = document.forms["kontaktform"]["kommentar"].value;
 		if (kfelt == null || kfelt == ""){
 			document.getElementById('tekstFelt').style.borderColor = "red";
--- Glowee/scripts/rollOverMenu.js
+++ Glowee/scripts/rollOverMenu.js
@@ -2,6 +2,7 @@ var menuItem;
 var timer;
 document.onclick = mouseClose;
 
+// Opens mouse over menu
 function mouseOpen(id) {
 	console.log(id)
 	if (menuItem) menuItem.style.visibility = "hidden";
@@ -9,14 +10,17 @@ function mouseOpen(id) {
 	menuItem.style.visibility = "visible";
 }
 
+// Closes the mouse over menu after 500 ms
 function mouseClose() {
 	timer = window.setTimeout(close, 500);
 }
 
+// Closes the mouse over menu
 function close() {
 	if (menuItem) menuItem.style.visibility = "hidden";
 }
 
+// Keeps the menu open
 function keepSubmenu() {
 	window.clearTimeout(timer);
 }
